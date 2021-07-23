961f33487da12301deb99fbfc6759aa5ffaf4ee7
ryonasso <ryounaoyuki@gmail.com>
2021-07-23 23:57:05 +0900

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { StripeModule } from './stripe/stripe.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    StripeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
